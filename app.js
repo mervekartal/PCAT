35f63cc81af790383f44135c8d43169b5665ec22
merve kartal <mervekartal@merves-MacBook-Air.local>
2022-12-15 17:46:53 +0300

    app.js and package files modified for middleware

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const fn = function() {
-    console.log("Function working")
-}
+const express = require('express');
+const app = express();
+
+// const myLogger = (req, res, next) => {
+//   console.log("middleware log")
+//   next()
+// }
+
+//middlewares
+//statik dosyaları(css html gibi) public klasörüne taşındı
+app.use(express.static('public'))
+// app.use(myLogger)
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'photo name',
+    description: 'description',
+  }
+  res.send(photo)
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
