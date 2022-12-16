da0b0ce12ac5e18f5c4200d80c7498f2313c2fb1
merve kartal <mervekartal@merves-MacBook-Air.local>
2022-12-16 12:51:08 +0300

    partial folder and ejs files created

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path')
+const ejs = require('ejs')
 
 const app = express();
+
+//Template Engine
+app.set("view engine", "ejs")
 
 // const myLogger = (req, res, next) => {
 //   console.log("middleware log")
@@ -13,9 +17,18 @@ const app = express();
 app.use(express.static('public'))
 // app.use(myLogger)
 
+//route
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  //res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render('index')
 })
+app.get('/add', (req, res) => {
+  res.render('add')
+})
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
 
 const port = 3000;
 app.listen(port, () => {
