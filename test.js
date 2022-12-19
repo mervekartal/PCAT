ad27ee7a73fd20f5e309e4a573e6974e898e8b1c
merve kartal <mervekartal@merves-MacBook-Air.local>
2022-12-19 17:24:41 +0300

    mongoose module added

---
+++ test.js
@@ -0,0 +1,8 @@
+//mongoose kullanmak için oluşturulan test dosyası
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema //Schema yapısı sayesinde türetilen nesneleri mongoDB içerisindeki dokümanlara dönüştürür.
+
+
+
+
