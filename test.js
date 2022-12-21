fd0b853298efd61a753965b38edb544a4243e10b
merve kartal <mervekartal@merves-MacBook-Air.local>
2022-12-21 13:51:37 +0300

    insert test data for mongodb

--- test.js
+++ test.js
@@ -4,5 +4,25 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema //Schema yapısı sayesinde türetilen nesneleri mongoDB içerisindeki dokümanlara dönüştürür.
 
 
+//connect db
+mongoose.connect('mongodb://localhost:27017/pcat-test-db')
+
+process.on('warning', (warning) => {
+    console.log(warning.stack);
+})
+
+//create schema
+const PhotoSchema = new Schema({
+    title: String,
+    description: String
+})
+
+const Photo = mongoose.model('Photo',PhotoSchema)
+
+//create photo
+Photo.create({
+    title: "Photo Test 2",
+    description: "Photo description test 2"
+})
 
 
