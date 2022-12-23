cff43b2afd820378703bfc9edae6e8e8f5f2c821
merve kartal <mervekartal@merves-MacBook-Air.local>
2022-12-23 08:53:46 +0300

    delete photo

--- app.js
+++ app.js
@@ -36,7 +36,9 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(fileUpload())
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}))
 
 //routes
 app.get('/', async (req, res) => {
@@ -100,6 +102,15 @@ app.put('/photos/:id', async (req, res) => {
   photo.save()
 
   res.redirect(`/photos/${req.params.id}`)
+})
+
+//delete photo
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({_id: req.params.id})
+  let deletedImage = __dirname + '/public' + photo.image
+  fs.unlinkSync(deletedImage)
+  await Photo.findByIdAndRemove(req.params.id)
+  res.redirect('/')
 })
 
 const port = 3000;
