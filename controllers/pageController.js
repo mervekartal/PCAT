66051aded5d8d5fd37928d28136412a7f3735dc0
merve kartal <mervekartal@merves-MacBook-Air.local>
2023-01-24 13:48:36 +0300

    typo

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -2,11 +2,11 @@ const Photo = require('../models/Photo')
 
 //routing operations
 exports.getAboutPage =  (req, res) => {
-    res.render('add')
+    res.render('about')
 }
 
 exports.getAddPage = (req, res) => {
-    res.render('about')
+    res.render('add')
 }
 
 exports.getEditPage = async (req, res) => {
